9767ac9c51731b40474e9d158c8a340fa93ccf18
Dawid Kozak <d.kozak92@gmail.com>
2016-06-26 14:23:02 +0200

    Add paste showing and deleting

--- paste/urls.py
+++ paste/urls.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import include, url
 from . import views
-from .views import PasteCreateView, PasteListView
+from .views import PasteCreateView, PasteListView, PasteShowView, PasteDeleteView
 from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     url(r'^add/$', PasteCreateView.as_view(), name='add_paste'),
     url(r'^list/$', PasteListView.as_view(), name='paste_list'),
+    url(r'^(?P<paste_id>\d+)/$', PasteShowView.as_view(), name='show_paste'),
+    url(r'^d/(?P<pk>\d+)/$', PasteDeleteView.as_view(), name='delete_paste'),
 ]
--- paste/views.py
+++ paste/views.py
@@ -8,8 +8,7 @@ from .models import Paste
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
-# Create your views here.
-
+from django.shortcuts import get_object_or_404
 
 class PasteCreateView(CreateView):
     model = Paste
@@ -18,10 +17,37 @@ class PasteCreateView(CreateView):
     success_url = reverse_lazy('main_page')
 
 
+class UserToolsFormView(UpdateView):
+    model = Paste
+    fields = ['title', 'content', 'visibility']
+    template_name = "paste/form.html"
+    success_url = reverse_lazy('main_page')
+
+
+class PasteDeleteView(DeleteView):
+
+    model = Paste
+    template_name = "paste/confirm_delete.html"
+
+    def get_success_url(self):
+        return reverse_lazy('paste_list')
+
+
+class PasteShowView(View):
+    template_name = "paste/show.html"
+
+    def get(self, request, paste_id):
+        paste = get_object_or_404(Paste, pk=paste_id)
+
+        return render(request, self.template_name, {
+            'paste': paste,
+        })
+
+
 class PasteListView(View):
     template_name = "paste/list.html"
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         return render(request, self.template_name, {
             'all_paste': Paste.objects.all()
         })
