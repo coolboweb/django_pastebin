18a9b8bf5366ce6af9824328a7cead812cfad5dd
Dawid Kozak <d.kozak92@gmail.com>
2016-06-29 18:33:31 +0200

    Change main page to paste add

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
-
+from paste.views import PasteCreateView
 urlpatterns = [
-    url(r'^$', views.main_view, name='main_page'),
+    url(r'^$', PasteCreateView.as_view(), name='main_add_page'),
+    url(r'^main/$', views.main_view, name='main_page'),
 ]
